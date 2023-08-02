a0377d9103627b0cb0839289282ec15bd1ffe144
Ravenna Oliveira <ravenna.rro@gmail.com>
2023-08-02 09:49:51 -0300

    init version

--- nbdev_example/card.py
+++ nbdev_example/card.py
@@ -59,3 +59,9 @@ def __gt__(self:Card, a:Card): return (self.suit, self.rank)>(a.suit, a.rank)
 
 # %% ../nbs/00_cards.ipynb 1
 #|export card
+
+# %% ../nbs/00_cards.ipynb 1
+#|export card
+
+# %% ../nbs/00_cards.ipynb 1
+#|export card
